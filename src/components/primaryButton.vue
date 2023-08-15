92c948f11baf9ce3a9663ccf9f88cc49af7d74f5
JailsonJr-BR <jailsonjunior513@gmail.com>
2023-08-15 17:05:03 -0300

    feat: add primary button component

---
+++ src/components/primaryButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button
+    type="button"
+    class="rounded-md w-full bg-primary px-3.5 py-3 text-lg font-semibold text-white shadow-sm hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-300"
+  >
+    {{ name }}
+  </button>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const { name } = defineProps({
+  name: {
+    type: String,
+    default: 'Clique'
+  }
+})
+</script>
