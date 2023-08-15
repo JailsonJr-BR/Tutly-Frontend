81fec2c560f22b3fc13ccbd3244400f6aaf597d8
JailsonJr-BR <jailsonjunior513@gmail.com>
2023-08-15 17:06:21 -0300

    chore: add index for components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import inputForm from '@/components/inputForm.vue'
+import cardSocialLogin from '@/components/cardSocialLogin.vue'
+import primaryButton from '@/components/primaryButton.vue'
+
+export { inputForm, cardSocialLogin, primaryButton }
--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="text-xl">Initial Setup</h1>
+  <h1 class="text-3xl text-primary">Tutly App</h1>
 </template>
 
 <script setup>
