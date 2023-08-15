b5ac93e1dd9e840f5d95c6f1054e52b9b47d6bf7
JailsonJr-BR <jailsonjunior513@gmail.com>
2023-08-15 17:02:28 -0300

    chore: add and update initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="font-sans subpixel-antialiased">
+  <div class='font-sans subpixel-antialiased'>
     <router-view />
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', 'sans-serif']
+      },
+      colors: {
+        primary: '#2EADF4',
+        'secondary-900': '#162B46',
+        'secondary-300': '#374151'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
