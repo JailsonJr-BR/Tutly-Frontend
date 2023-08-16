e596f8f1a2a6259e18cd89788cbc9c41a161000e
Jailson Silva <104029921+JailsonJr-BR@users.noreply.github.com>
2023-08-16 20:57:21 -0300

    feat: add divider text component

---
+++ src/components/dividerText.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="relative">
+    <div class="absolute inset-0 flex items-center" aria-hidden="true">
+      <div class="w-full border-t border-gray-300" />
+    </div>
+    <div class="relative flex justify-center">
+      <span class="bg-white px-2 text-md text-gray-500">{{ name }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue';
+
+  const { name } = defineProps({
+    name: {
+      type: String,
+      default: '',
+      required: true
+    }
+  })
+</script>
