eebc1ed6d3a8d50d99bafb0ce372b0037e61decd
Jailson Silva <104029921+JailsonJr-BR@users.noreply.github.com>
2023-08-16 20:57:53 -0300

    feat: add text link component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 import inputForm from '@/components/inputForm.vue'
 import cardSocialLogin from '@/components/cardSocialLogin.vue'
 import primaryButton from '@/components/primaryButton.vue'
+import dividerText from '@/components/dividerText.vue'
+import checkboxDescription from '@/components/checkboxDescription.vue'
+import textLink from '@/components/textLink.vue'
 
-export { inputForm, cardSocialLogin, primaryButton }
+export { inputForm, cardSocialLogin, primaryButton, dividerText, checkboxDescription, textLink }
---
+++ src/components/textLink.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    <a :href="link" class="text-md text-primary font-bold">{{ name }}</a>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const { name, link } = defineProps({
+  name: {
+    type: String,
+    default: '',
+    required: true
+  },
+  link: {
+    type: [String, Object, Function],
+    default: null,
+    required: true
+  }
+})
+</script>
